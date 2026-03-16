e14cb8969d1d5c8520e2644fae5ed0f0300e66e4
SirinWasHere <vovasukachevkill@gmail.com>
2026-03-16 09:50:24 +0500

    Вынос переменных
    Длинный метод - это метод выполняющий всё и сразу
    
    Я выбрал данных метод, так как он позволит снизить количество строк в уже и так заграмождённом методе.
    
    После данного рефакторинга, провести последующий рефакторинг длинного метода, будет в разы легче

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -2,14 +2,8 @@ import { useEffect, useState, useMemo, useCallback } from 'react'
 import { api, buildEndpoint } from '../api/endpoints.js'
 import { useAuth } from '../state/AuthContext.jsx'
 import { formatNumber, formatCurrency, isValidDate } from '../utils/format.js'
-import { PAGINATION, ERROR_MESSAGES } from '../constants.js'
+import { PAGINATION, ERROR_MESSAGES, MIN_DIMENSION, MIN_WEIGHT, MAX_DIMENSION, MAX_WEIGHT, CM_TO_M, emptyProduct } from '../constants.js'
 
-
-const MIN_WEIGHT = 0.01
-const MAX_WEIGHT = 10000
-const MIN_DIMENSION = 0.1
-const MAX_DIMENSION = 500
-const CM_TO_M = 100
 
 
 function calculateVolume(length, width, height) {
